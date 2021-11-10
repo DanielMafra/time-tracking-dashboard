3ec17b2947dd83b2e74e78b8aedae011ae183423
Daniel Mafra <danielmafradev@gmail.com>
2021-11-10 11:07:36 -0300

    First commit

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Home.module.css';
+
+const Home = () => {
+  return (
+    <div>
+      Frontend Mentor Challenge
+    </div>
+  );
+};
+
+export default Home;
